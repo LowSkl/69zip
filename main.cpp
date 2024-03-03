cd25ea571b0209b8c4b7659c8de51db35b9fe66b
LowSkl <olfegr3_0@mail.ru>
2024-03-03 23:08:53 +0300

    potehoniky

--- main.cpp
+++ main.cpp
@@ -1,6 +1,12 @@
 ﻿#include <iostream>
 
-int main()
-{
+const enum EXIT_STATUS {
+    UNKNOWN_ERROR = -1,
+    SUCCESSFUL = 0,
+};
+
+int main(int argc, char *argv[]) {
     std::cout << "Hello World!\n";
+
+    return EXIT_STATUS::SUCCESSFUL();
 }
