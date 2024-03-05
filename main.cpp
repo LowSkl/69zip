c7091a6503c9af5f58e2966c40ee45b76cb6b301
Bruhbyy <electroytprofessional@gmail.com>
2024-03-05 21:08:48 +0300

    НИХУЯ

--- main.cpp
+++ main.cpp
@@ -1,6 +1,34 @@
-﻿#include <iostream>
+﻿#include "stdafx.h"
 
-int main()
+
+class Program {
+public:
+    fs::path workingDirectory;
+    fs::path compressedFile;
+
+    
+
+
+public:
+    void GetWorkingDirectory()
+    {
+        workingDirectory = fs::current_path();
+    }
+    void GetResultFile() 
+    {
+        compressedFile = workingDirectory / "COMPRESSED.txt";
+        std::ofstream file(compressedFile, std::ofstream::out | std::ofstream::trunc);
+        file.close();
+    }
+
+};
+
+int main() 
 {
-    std::cout << "Hello World!\n";
+    std::string inputFileName;
+    std::cout << "Make sure your .txt file in same directory as compressor!\n";
+    std::cout << "Write your file name: ";
+    std::cin >> inputFileName;
+    Program idkName;
+    idkName.GetResultFile();
 }
